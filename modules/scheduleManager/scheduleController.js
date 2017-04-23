caaece0fa8d0bc134368b24fa566c5108683b3b6
HonzoBonzo <konradb12@gmail.com>
2017-04-23 15:29:05 +0200

    added parser txt to json schedule

--- modules/scheduleManager/scheduleController.js
+++ modules/scheduleManager/scheduleController.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var scheduleService = require('./scheduleService');
+var _ = require('lodash');
 
 module.exports = {
 	getResultJson: getResultJson
@@ -11,9 +12,54 @@ function getResultJson(req, res, next) {
 
 	fs.readFile(filePath, 'utf8', function(err, data) {
 		if (err) {throw err;}
-		console.log("wczytalem plik");
-		res.send(data);
+		console.log("loaded file: ", filePath);
+
+		//parse
+		var rows = _.split(data, '\r\n', 2240);
+		rows = _.map(rows, row => {
+			return _.split(row, ' ', 140);
+		});
+		
+		rows = _.map(rows,  row => {
+			var tab = [];
+
+			for (var i = 0; i < 140; i++) {
+				if( row[i] === '1' ) {
+					tab.push( makeDay(i) );
+				} else if( i % 4  === 3 && tab[scheduleService.getDayId(i)] === undefined) {
+					tab.push( makeBlankDay(i) );
+				}
+			}
+
+			return tab
+		})
+
+		var i = -1;
+		var resultJson = _.map(rows, row => {
+			i = i + 1;
+			return {
+				nurseId: i,
+				name: 'Baśka'+i,
+				days: row 
+			}
+		})
+
+		res.send(resultJson);
 	})
-	
-	// res.send("nothing here");
+}
+
+function makeDay(i) {
+	var day = {
+		dayId: scheduleService.getDayId(i),
+		signature: scheduleService.getShiftSignature(i)
+	}
+	return day;
+}
+
+function makeBlankDay(i) {
+	var day = {
+		dayId: scheduleService.getDayId(i),
+		signature: "0"
+	}
+	return day;
 }
--- modules/scheduleManager/scheduleService.js
+++ modules/scheduleManager/scheduleService.js
@@ -1,9 +1,12 @@
 var Promise = require('bluebird');
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 
 module.exports = {
 	parseTxtToJson: parseTxtToJson,
-	getFilePath: getFilePath
+	getFilePath: getFilePath,
+	getShiftSignature: getShiftSignature,
+	getDayId: getDayId
 }
 
 function parseTxtToJson() {
@@ -13,3 +16,29 @@ function parseTxtToJson() {
 function getFilePath() {
 	return './wyniki/5000/tab.txt';
 }
+
+function getShiftSignature(number) {
+	var signature = "";
+	switch(number%4) {
+		case 0: 
+			signature = 'E';
+			break;
+		case 1: 
+			signature = 'D';
+			break;
+		case 2: 
+			signature = 'L';
+			break;
+		case 3: 
+			signature = 'N';
+			break;
+		default:
+			signature = "0";
+	}
+	return signature;
+}
+
+function getDayId(number) { // 0 - 34
+	return _.floor(number/4);
+}
+
