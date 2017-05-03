1a766dc0a4134663e9b08fc45b5309c6c145137b
HonzoBonzo <konradb12@gmail.com>
2017-05-03 14:29:56 +0200

    fix

--- app.js
+++ app.js
@@ -7,30 +7,28 @@ var cookieParser = require('cookie-parser');
 var scheduleManagerRouter = require('./modules/scheduleManager/scheduleRouter');
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Methods", 'POST, GET, PUT, DELETE, OPTIONS');
-    res.header("Access-Control-Allow-Credentials", true);
-
     next();
 });
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(expressSession({
-	secret: 'secret',
-	resave: false,
-	saveUninitialized: true,
-	maxAge: 10000000000000
-}));
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+// app.use(bodyParser.json());
+// app.use(cookieParser());
+// app.use(expressSession({
+// 	secret: 'secret',
+// 	resave: false,
+// 	saveUninitialized: true,
+// 	maxAge: 10000000000000
+// }));
 
-process.on('unhandledRejection', function(reason, p){
-    console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
-    // application specific logging here
-});
+// process.on('unhandledRejection', function(reason, p){
+//     console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
+//     // application specific logging here
+// });
 
 app.get('/', function (req, res) {
   res.send('NurseSchedulingServer!')
