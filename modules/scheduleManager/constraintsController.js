fecd41d7f2fe2e0b2661efc2dcd54ffdd4a34336
HonzoBonzo <konradb12@gmail.com>
2017-05-06 14:40:48 +0200

    fix

--- modules/scheduleManager/constraintsController.js
+++ modules/scheduleManager/constraintsController.js
@@ -10,25 +10,25 @@ function getConstraints(req, res, next) {
   const mockConstraints = [
     { 
       failedHardSum: 0,
-      failedHards: {
-        hard1: 0,
-        hard2: 0,
-        hard3: 0,
-        hard4: 0,
-        hard5: 0,
-        hard6: 0
-      }
+      failedHards: [
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+      ]
     },
     {
       failedSoftSum: 4748,
-      failedSofts: {
-        soft1: 934,
-        soft2: 2567,
-        soft3: 123,
-        soft4: 1137,
-        soft5: 456,
-        soft6: 235
-      }
+      failedSofts: [
+        934,
+        2567,
+        123,
+        1137,
+        456,
+        235
+      ]
     }
   ];
 
