fc5fe0745b8cfc96c153abe3b9f4a9ce667b64d5
HonzoBonzo <konradb12@gmail.com>
2017-06-04 20:03:17 +0200

    added logs

--- modules/scheduleManager/integrationController.js
+++ modules/scheduleManager/integrationController.js
@@ -9,12 +9,14 @@ module.exports = {
 }
 
 function saveFirstWeek(req, res, next) {
+    console.log('saving file...');
     const week = req.body.week 
     fs.writeFile('firstWeek.txt', week, 'utf8', function(err) {});
     res.send(req.body);
 }
 
 function runAll(req, res, next) {
+    console.log('running jar...');
     var msg = {};
     exec('java -jar ./nsp_2.0.jar');
     msg = {code: 200, status: "OK"};
